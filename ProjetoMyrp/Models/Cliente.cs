5510b743046f03537ce933dced0583f8f713f9a0
matheus.cuchi <matheus.cuchi@outlook.com>
2024-10-03 21:50:19 -0300

    Padronizada a estrutura de pastas

---
+++ ProjetoMyrp/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoMyrp.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(14, MinimumLength = 11)]
+        public string CPFouCNPJ { get; set; } // CPF (11 dígitos) ou CNPJ (14 dígitos)
+    }
+}
